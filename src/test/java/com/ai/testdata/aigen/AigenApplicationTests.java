ebf88c2ac959179ff47a78da5fd5d51b8564451b
Frederic Gelat <fgelat@hotmail.fr>
2025-10-19 11:20:53 +0200

    Initial commit - Spring Boot project setup

---
+++ src/test/java/com/ai/testdata/aigen/AigenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.testdata.aigen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AigenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
